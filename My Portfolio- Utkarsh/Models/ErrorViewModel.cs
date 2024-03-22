4ee4e9da3690f2d47eea1d2ac114b5962b42e785
Utkarshbhatt64 <utkarshbhatt64@gmail.com>
2024-03-22 22:21:21 +0530

    Add project files.

---
+++ My Portfolio- Utkarsh/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_Portfolio__Utkarsh.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
